ec5c09c474bcd97c13dc050dc0d774c31c6dfca0
Nagireddy <mulisivanagireddy@gmail.com>
2025-03-11 08:45:59 +0530

    new Code-011-03-2025

---
+++ src/ex_01_Java_Basic/Excercise_Printf01.java
@@ -0,0 +1,29 @@
+package ex_01_Java_Basic;
+
+public class Excercise_Printf01 {
+    public static void main(String[] args) {
+        int i = 9, count=1;
+
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+        System.out.printf("%d * %d = %d\n", i, count, (i * count));
+        count++;
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Excercise_Printf02.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+public class Excercise_Printf02 {
+
+    public static void main(String[] args) {
+        int i = 9, count = 1;
+        System.out.printf("%d * %d = %d\n", i, 1, (i));
+        System.out.printf("%d * %d = %d\n", i, 2, (i * 2));
+        System.out.printf("%d * %d = %d\n", i, 3, (i * 3));
+        System.out.printf("%d * %d = %d\n", i, 4, (i * 4));
+        System.out.printf("%d * %d = %d\n", i, 5, (i * 5));
+        System.out.printf("%d * %d = %d\n", i, 6, (i * 6));
+        System.out.printf("%d * %d = %d\n", i, 7, (i * 7));
+        System.out.printf("%d * %d = %d\n", i, 8, (i * 8));
+        System.out.printf("%d * %d = %d\n", i, 9, (i * 9));
+        System.out.printf("%d * %d = %d\n", i, 10, (i * 10));
+
+
+    }
+}
