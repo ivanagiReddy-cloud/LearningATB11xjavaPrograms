556c60f54a9afb0222f3e177c6bbb0bd99581c55
Nagireddy <mulisivanagireddy@gmail.com>
2025-03-13 08:31:05 +0530

    new Code-13-03-2025(2)

---
+++ src/ex_01_Java_Basic/TernaryOperatorExcercise01.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class TernaryOperatorExcercise01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Score Value: ");
+        int Score = scanner.nextInt();
+
+        String grade = (Score >= 90) ? "A" : (Score >= 80) ? "B" : (Score >= 70) ? "C" : (Score >= 60) ? "D" : "F";
+
+        System.out.println(grade);
+    }
+}
